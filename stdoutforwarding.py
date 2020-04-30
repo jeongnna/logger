d20b741e11040320236a19f5ad095ee6ca9257b5
Jeongmin Kim <jeongnna@Jeongminui-MacBookAir.local>
2020-04-30 14:27:34 +0900

    Update

--- stdoutforwarding.py
+++ stdoutforwarding.py
@@ -2,13 +2,15 @@ import sys
 import logging
 
 
-logger = logging.getLogger('stdout_forwarding')
-logger.setLevel(logging.INFO)
+fw_logger = logging.getLogger('stdout_forwarding')
+fw_logger.setLevel(logging.INFO)
 
 
-class LogWriter:
+class LoggerWriter:
 
-    def __init__(self):
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
         self._linebuf = ''
 
     def write(self, buf):
@@ -17,31 +19,32 @@ class LogWriter:
 
         for line in temp_linebuf.splitlines(keepends=True):
             if line[-1] == '\n':
-                logger.info(line.rstrip())
+                self.logger.log(self.log_level, line.rstrip())
             else:
                 self._linebuf += line
 
     def flush(self):
         if self._linebuf != '':
-            logger.info(self._linebuf.rstrip())
+            self.logger.log(self.log_level, self._linebuf.rstrip())
+
         self._linebuf = ''
 
 
-_logwriter = LogWriter()
+_loggerwriter = LoggerWriter(fw_logger)
 _stdout = sys.stdout
 _stderr = sys.stderr
 
 
 def start(logfile):
-    logger.addHandler(logging.StreamHandler())
-    logger.addHandler(logging.FileHandler(logfile))
+    fw_logger.addHandler(logging.StreamHandler())
+    fw_logger.addHandler(logging.FileHandler(logfile))
 
-    sys.stdout = _logwriter
-    sys.stderr = _logwriter
+    sys.stdout = _loggerwriter
+    sys.stderr = _loggerwriter
 
 
 def stop():
-    logger.handlers = []
+    fw_logger.handlers = []
 
     sys.stdout = _stdout
     sys.stderr = _stderr
